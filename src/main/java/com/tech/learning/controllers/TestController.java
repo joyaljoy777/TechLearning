71fa261b88bab128fa0a1e589ecadaaecb34da8a
Joyal Joy <joyal.joy@mdoc.one>
2022-09-17 22:26:39 +0200

    Added hello world get method

--- src/main/java/com/tech/learning/controllers/TestController.java
+++ src/main/java/com/tech/learning/controllers/TestController.java
@@ -3,17 +3,23 @@ package com.tech.learning.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/test")
 public class TestController {
 
-	@GetMapping("/msg")
+	@GetMapping("/test/msg")
 	public ResponseEntity<String> getAllUsers() {
 		
 		return ResponseEntity.ok("Test Message from spring boot application - TechLearning");
+	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String helloWorld(){
+		return "Hello World From GKE Via Github Actions!";
 	}
 
 }
