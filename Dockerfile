618a8c2ac6f69d8ae97e084ac4c09cdc68a7c35e
Joyal Joy <joyal.joy@mdoc.one>
2022-09-17 21:40:55 +0200

    deployment pipeline setup using git action and gke

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM openjdk:11-jdk-alpine
+
+EXPOSE 8889
+
+ARG JAR_FILE=target/learning-0.0.1-SNAPSHOT.jar
+ADD $(JAR_FILE) app-jar
+
+ENTRYPOINT exec java -jar /app.jar
--- src/main/java/com/tech/learning/controllers/TestController.java
+++ src/main/java/com/tech/learning/controllers/TestController.java
@@ -1,6 +1,9 @@
 package com.tech.learning.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
